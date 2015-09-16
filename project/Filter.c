b55b53e0b319b5541bc7d339e9ded737a97b8f7f
anhvan <anhvan2505@yahoo.com>
2015-09-16 15:52:27 +0200

    Filters fixed

--- project/Filter.c
+++ project/Filter.c
@@ -1,52 +1,30 @@
-
 #include <stdio.h>
 #include "Filter.h"
 
-void filter(int data, int x[], int y[]){
-	int N = 30;
-	int lowPass[33];
-	int highPass[5];
-	int derivative[31];
-	int squaring[31];
-
-	static int n = 0;
-
-	x[n%13] = data;
-
-	lowpass(x, lowPass, 13, 33, n);
-	highpass(lowPass, y, 33, 5, n);
-	//derivative();
-	//squaring();
-	//mwi();
-	n++;
-
-}
-void lowpass(int x[], int y[], int inputSize, int outputSize, int n){
+void lowpass(int x[], int *y, int inputSize, int outputSize, int n){
 	y[n%outputSize] = (2*y[(n-1+outputSize)%outputSize]-y[(n-2+outputSize)%outputSize]+(x[n%inputSize]-2*x[(n-6+inputSize)%inputSize]+x[(n-12+inputSize)%inputSize])/32);
 }
 
 void highpass(int x[], int y[], int inputSize, int outputSize, int n){
 	y[n%outputSize]=y[(n-1+outputSize)%outputSize]-x[n%inputSize]/32+x[(n-16+inputSize)%inputSize]-x[(n-17+inputSize)%inputSize]+x[(n-32+inputSize)%inputSize]/32;
-	printf("n = %d, x = %d, y = %d\n", n+1, x[n%inputSize],y[n%outputSize]);
-}
-/*
-derivative(){
-	y3[n]=1/8.0*(2*y2[n]+y2[(n-1+size)%size]-y2[(n-3+size)%size]-2*y2[(n-4+size)%size]);
 }
 
-squaring(){
-	y4[n] = y3[n]*y3[n];
+derivative(int x[], int y[], int inputSize, int outputSize, int n){
+	y[n%outputSize]=(2*x[n%inputSize]+x[(n-1+inputSize)%inputSize]-x[(n-3+inputSize)%inputSize]-2*x[(n-4+inputSize)%inputSize])/8;
 }
 
-mwi(){
+
+squaring(int x[], int y[], int inputSize, int outputSize, int n){
+	y[n%outputSize] = x[n%inputSize]*x[n%inputSize];
+}
+
+movingWindow(int x[], int y[], int inputSize, int outputSize, int n){
 	int ytemp = 0;
+	int N = 30;
 	for(int i = 1; i <= N; i++){
-		ytemp += (y4[(n-(N-i)+size)%size]);
+		ytemp += x[(n-(N-i)+inputSize)%inputSize];
 	}
-	//matches x_mwi_div_after file
-	y5[n] = ytemp/N;
+	y[n%outputSize] = ytemp/N;
 
 }
-*/
-
 
--- project/main.c
+++ project/main.c
@@ -5,22 +5,21 @@
 
 
 int main(void) {
-	//const int size = 50;
 	int data[13]={0};
-	int lowPass[33]={0};
-	int movingWindow[50]={0};
-	int n = 0;
+	int lowPass[33] = {0};
+	int highPass[5] = {0};
+	int deriv[31] = {0};
+	int squar[31] = {0};
+	int mwi[50]={0};
 
-	int j = 0;
 	for(int i = 0; i<55; i++){
-		filter(getNextData(), data, lowPass);
-		if(i == 50){
-			j=0;
-		}
-		//printf("%d, ", j);
-		//printf("%d, ", x[j]);
-		//printf("%d\n", y[j]);
-		j++;
+		data[i%13] = getNextData();
+		lowpass(data, lowPass, 13, 33, i);
+		highpass(lowPass, highPass, 33, 5, i);
+		derivative(highPass, deriv, 5, 31, i);
+		squaring(deriv, squar, 31, 31, i);
+		movingWindow(squar, mwi, 31, 50, i);
+		printf("n = %d, x = %d, y = %d\n", i+1, squar[i%31], mwi[i%50]);
 	}
 	return 0;
 }
